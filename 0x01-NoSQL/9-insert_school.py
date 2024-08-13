c606b5f995cb61bf933e63d67bf9bfeedb7417c4
JuggernautTej <tiwatej@gmail.com>
2024-08-13 11:57:53 +0100

    populated task 9 file

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A Python function that inserts a new document in
+a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """This method lists all documents in a collection.
+    Args:
+        mongo_collection: a pymongo collection object.
+    Returns:
+        List: A list of all documents in the collection."""
+    result = mongo_collection.insert_one(**kwargs)
+    return result.inserted_ids
