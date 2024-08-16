ddfc2d393e6cad50e3992ed94a1358d8268dbb9d
JuggernautTej <tiwatej@gmail.com>
2024-08-16 18:55:52 +0100

    updated task file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -35,6 +35,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """A method that takes a data argument and returns
         a string.
